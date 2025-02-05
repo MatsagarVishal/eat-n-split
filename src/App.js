6decb8a3a71bf465caf53e6b298066c9c161bac6
Vishal Matsagar <vishalmatsagar0007@gmail.com>
2025-02-05 22:35:11 +0530

    adding states to Split bill form

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 function App() {
   const listOFfriends = [
     {
@@ -37,27 +38,35 @@ function Friends({ friendList }) {
     <div className="w-64 min-h-64 flex flex-col items-center justify-center">
       <ul className="w-full flex items-center justify-between flex-col">
         {friendList.map((friend) => (
-          <li className="w-full flex items-center justify-between flex-row mb-3 p-2 rounded-sm hover:bg-slate-300 ">
-            <img src={friend.image} alt="pic" className="w-11 rounded-full" />
-
-            <span>
-              {friend.name} {friend.balence}
-            </span>
-            <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-4 border border-blue-500 hover:border-transparent rounded">
-              select
-            </button>
-          </li>
+          <Friend friend={friend} key={friend.id} />
         ))}
       </ul>
     </div>
   );
 }
+function Friend({ friend }) {
+  return (
+    <li className="w-full flex items-center justify-between flex-row mb-3 p-2 rounded-sm hover:bg-slate-300 ">
+      <img src={friend.image} alt="pic" className="w-11 rounded-full" />
 
+      <span>
+        <h2>{friend.name}</h2>
+        {<p className="text-xs text-gray-500"></p>}
+      </span>
+      <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-4 border border-blue-500 hover:border-transparent rounded">
+        select
+      </button>
+    </li>
+  );
+}
 function AddFriendForm() {
   return (
     <div className="form ">
       <form className="flex flex-col w-64 space-y-4">
-        <label class="block text-gray-700 text-xs font-bold " for="username">
+        <label
+          className="block text-gray-700 text-xs font-bold "
+          htmlFor="username"
+        >
           Name Of Friend
         </label>
         <input
@@ -66,7 +75,10 @@ function AddFriendForm() {
           placeholder="friend name"
           className="p-2 text-xs border-2 "
         />
-        <label class="block text-gray-700 text-xs font-bold " for="photo">
+        <label
+          className="block text-gray-700 text-xs font-bold "
+          htmlFor="photo"
+        >
           Photo
         </label>
         <input
@@ -75,11 +87,11 @@ function AddFriendForm() {
           placeholder="friend image"
           className="p-2 text-sm border-2 rounded"
         />
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           Add
         </button>
       </form>
-      <button class="mt-3 flex m-auto bg-gray-300 hover:bg-blue-300 hover:text-white text-gray-700 font-bold py-2 px-4 rounded">
+      <button className="mt-3 flex m-auto bg-gray-300 hover:bg-blue-300 hover:text-white text-gray-700 font-bold py-2 px-4 rounded">
         open
       </button>
     </div>
@@ -87,14 +99,17 @@ function AddFriendForm() {
 }
 
 function FormSplitBill() {
+  // const [selectedFriend, setSelectedFriend] = useState(null);
+  const [totalBill, setTotalBill] = useState(0);
+  const [yourExpense, setYourExpense] = useState(0);
   return (
     <div className="form bg-orange-300 rounded p-3 w-96 flex items-center justify-center flex-col">
       <h1 className="font-bold text-xl">Split Bill with X</h1>
       <form className="form mt-4 space-y-5">
         <div className="grid grid-cols-2 gap-2 ">
           <label
-            class="block text-gray-800  text-sm font-bold "
-            for="titalSplit"
+            className="block text-gray-800  text-sm font-bold "
+            htmlFor="titalSplit"
           >
             Total Bill
           </label>
@@ -102,12 +117,14 @@ function FormSplitBill() {
             type="text"
             id="tatalSplit"
             className="p-1 text-sm border-2 rounded"
+            value={totalBill}
+            onChange={(e) => setTotalBill(+e.target.value)}
           />
         </div>
         <div className="grid grid-cols-2 gap-2  ">
           <label
-            class="block text-gray-800  text-sm font-bold "
-            for="yourExpense"
+            className="block text-gray-800  text-sm font-bold "
+            htmlFor="yourExpense"
           >
             Your expenses
           </label>
@@ -115,12 +132,14 @@ function FormSplitBill() {
             type="text"
             id="yourExpense"
             className="p-1 text-sm border-2 rounded"
+            value={yourExpense}
+            onChange={(e) => setYourExpense(+e.target.value)}
           />
         </div>
         <div className="grid grid-cols-2 gap-2  ">
           <label
-            class="block text-gray-800  text-sm font-bold "
-            for="friendsExpense"
+            className="block text-gray-800  text-sm font-bold "
+            htmlFor="friendsExpense"
           >
             X's expenses
           </label>
@@ -128,25 +147,22 @@ function FormSplitBill() {
             type="text"
             id="friendsExpense"
             className="p-1 text-sm border-2 rounded"
+            disabled
           />
         </div>
         <div className="grid grid-cols-2 gap-2  ">
           <label
-            class="block text-gray-800  text-sm font-bold "
-            for="whoPaying"
+            className="block text-gray-800  text-sm font-bold "
+            htmlFor="whoPaying"
           >
             Who is paying
           </label>
           <select id="whoPaying">
-            <option value="you" key="">
-              you
-            </option>
-            <option value="friend" key="">
-              friend
-            </option>
+            <option value="you">you</option>
+            <option value="friend">friend</option>
           </select>
         </div>
-        <button class="w-full bg-blue-300 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded">
+        <button className="w-full bg-blue-300 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded">
           Split Bill
         </button>
       </form>
